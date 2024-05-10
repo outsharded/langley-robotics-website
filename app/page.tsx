e72a926f022a0caff1cec1b70676854093e322da
outsharded <fab_tec@icloud.com>
2024-05-10 19:16:55 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="min-h-screen text-cyan-950 dark:text-amber-50 bg-stone-100 dark:bg-gray-800">
      <Header />
+     <link rel="icon" href="/favicon.ico" sizes="any" />
       <main className="p-1 mb:p-4 px-4 mb:px-10">
         <div>
           <h1 className="text-5xl font-bold mb-4">Team Powercut</h1>
@@ -27,12 +28,12 @@ const HomePage: React.FC = () => {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
                 </p>
               </div>
-              <div className="rounded-lg pl-1 lg:basis-1/5">
+              <div className="rounded-lg pl-1 basis-1/2 lg:basis-1/5">
                 <Image className="rounded-md" src="/robot_in_room.jpg" alt="Powercut Robotics Team" width={10000} height={100000} />
               </div>
             </div>
             <div className="flex p-3 dark:hover:bg-gray-700 hover:bg-gray-300 rounded-md">
-              <div className="rounded-lg pl-1 lg:basis-1/5">
+              <div className="rounded-lg pl-1 basis-1/2 lg:basis-1/5">
                 <Image className="rounded-md" src="/robot_in_room.jpg" alt="Powercut Robotics Team" width={10000} height={100000} />
               </div>
               <div className="basis-1/4 flex-1 px-4">
