e489cc44c13bfee2517ae4d8a830df3c0fcf488f
outsharded <fab_tec@icloud.com>
2024-05-11 10:49:13 +0100

    logo optimise

--- app/Header.tsx
+++ app/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
           <h1 className={`px-2 text-2xl font-bold align-middle font-mono ${isMenuOpen ? 'hidden' : ''}`}>Langley <br></br> Robotics</h1>
         </Link>
         <div className="hidden md:block logo w-72 p-4">
-          <Image src="/powercut-logo.png" alt="Powercut Robotics Team Logo" width={1000} height={136} sizes="(max-width: 768px) 33vw, (max-width: 1200px) 15vw, 15vw" />
+          <Image src="/powercut-logo.png" alt="Powercut Robotics Team Logo" width={2000} height={400} sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 33vw" />
         </div>
       </div>
       <div className="md:basis-3/5 flex justify-end w-full">
