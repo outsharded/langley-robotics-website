4f4c11c57b50f060b5bd46983cedcf21900e155a
outsharded <fab_tec@icloud.com>
2024-05-12 15:54:35 +0100

    theme colour

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import type { Viewport } from 'next'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,6 +9,13 @@ export const metadata: Metadata = {
   title: "Langley Robotics",
   description: "FTC Team #25268, Powercut",
 };
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#f5f5f4' },
+    { media: '(prefers-color-scheme: dark)', color: '#1f2937' },
+  ],
+}
 
 export default function RootLayout({
   children,
