a4de5eca87c94db4a76877303c2b12e136407a6d
outsharded <fab_tec@icloud.com>
2024-05-15 17:42:24 +0100

    Update Header.tsx

--- app/Header.tsx
+++ app/Header.tsx
@@ -50,7 +50,7 @@ const Header: React.FC = () => {
           
         </div>
         <div className="lg:hidden absolute top-8 right-4">
-          <button id='menu' onClick={toggleMenu} className="focus:outline-none">
+          <button id='menu' aria-label="menu" onClick={toggleMenu} className="focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
