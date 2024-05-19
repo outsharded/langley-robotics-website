eead1f3398a415d17119d621182b1daced049376
outsharded <fab_tec@icloud.com>
2024-05-19 15:22:06 +0100

    clean

--- app/(subpages)/team/page.tsx
+++ app/(subpages)/team/page.tsx
@@ -11,46 +11,46 @@ const HomePage: React.FC = () => {
         <div>
           <h1 className="text-5xl font-bold font-avenir mb-4 px-4">Meet the team</h1>
           <div className="">
-            <div className='grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-2 items-center'>
+            <div className='grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 p-2 items-center justify-center'>
               
               <div className='relative rounded-lg overflow-hidden'>
                 <div className="">
-                  <Image className='rounded-lg' src="/headshots/fabian.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
+                  <Image className='rounded-lg w-full' src="/headshots/fabian.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
                 </div>
-                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Fabian</div>
-                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Programming Lead</div>
+                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Fabian</div>
+                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Programming Lead</div>
               </div>
 
               <div className='relative rounded-lg overflow-hidden'>
                 <div className="">
-                  <Image className='rounded-lg' src="/headshots/gupta.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
+                  <Image className='rounded-lg w-full' src="/headshots/gupta.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
                 </div>
-                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Adrian</div>
-                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Outreach and Strategy Lead</div>
+                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Adrian</div>
+                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Outreach and Strategy Lead</div>
+              </div>
+
+              <div className='relative rounded-lg overflow-hidde'>
+                <div className="">
+                  <Image className='rounded-lg w-full' src="/headshots/arthur.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
+                </div>
+                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Arthur</div>
+                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Engineering Lead</div>
               </div>
 
               <div className='relative rounded-lg overflow-hidden'>
                 <div className="">
-                  <Image className='rounded-lg' src="/headshots/arthur.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
+                  <Image className='rounded-lg w-full' src="/headshots/keiran3.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
                 </div>
-                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Arthur</div>
-                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Engineering Lead</div>
+                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Keiran</div>
+                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Finance Lead</div>
               </div>
 
               <div className='relative rounded-lg overflow-hidden'>
                 <div className="">
-                  <Image className='rounded-lg' src="/headshots/keiran3.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
+                  <Image className='rounded-lg w-full' src="/headshots/teddy.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
                 </div>
-                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Keiran</div>
-                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Finance Lead</div>
-              </div>
-
-              <div className='relative rounded-lg overflow-hidden'>
-                <div className="">
-                  <Image className='rounded-lg' src="/headshots/teddy.png" alt="Powercut Robotics Team" width={500} height={500} sizes="(max-width: 768px) 50wv, (max-width: 1024px) 33vw, 100vw"/>
-                </div>
-                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Teddy</div>
-                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-gray-800 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Merchandise Officer</div>
+                <div className="absolute bottom-12 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-r-lg font-mono text-2xl px-2 ">Teddy</div>
+                <div className="absolute bottom-0 left-0 bg-stone-100 dark:bg-neutral-950 bg-opacity-70 dark:bg-opacity-70 rounded-tr-lg text-xl p-2">Merchandise Officer</div>
               </div>
 
             </div>
@@ -64,3 +64,4 @@ const HomePage: React.FC = () => {
 };
 
 export default HomePage;
+
--- app/Footer.tsx
+++ app/Footer.tsx
@@ -6,21 +6,23 @@ const Footer: React.FC = () => {
     <footer className='font-avenir grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2 w-full text-md'>
 
         <div className='bg-stone-200 dark:bg-neutral-950 h-32 m-2 rounded-lg'>
-          <h1 className='text-6xl p-2 font-bold'>Contact Us</h1>
-          
-          <a href="mailto:conatct@powercut-robotics.org" className='text-xl px-2 py-1'>Contact: contact@powercut-robotics.org</a><br/>
+          <h1 className='text-5xl p-2 font-bold'>Contact Us</h1>
+          <div className='px-2'>
+          <a href="mailto:conatct@powercut-robotics.org" className='text-xl py-1'>Contact: contact@powercut-robotics.org</a><br/></div>
         </div>
 
         <div className='bg-stone-200 dark:bg-neutral-950 h-32 m-2 rounded-lg'>
-            <h1 className='text-6xl p-2 font-bold'>Finance</h1>
-            <a href="mailto:finance@powercut-robotics.org" className='text-xl px-2 py-1'>Finance: finance@powercut-robotics.org</a><br/>
+            <h1 className='text-5xl p-2 font-bold'>Finance</h1>
+            <div className='px-2'>
+            <a href="mailto:finance@powercut-robotics.org" className='text-xl py-1'>Finance: finance@powercut-robotics.org</a><br/></div>
         </div>
 
         <div className='bg-stone-200 dark:bg-neutral-950 h-32 m-2 rounded-lg'>
-            <h1 className='text-6xl p-2 font-bold'>Socials</h1>
-            <a href="https://www.instagram.com/powercut_robotics/" className='text-xl px-2 py-1'>Instagram</a><br/>
+            <h1 className='text-5xl p-2 font-bold'>Socials</h1>
+            <div className='px-2'>
+            <a href="https://www.instagram.com/powercut_robotics/" className='text-xl py-1'>Instagram</a><br/></div>
         </div>
-      <p className='m-2 font-avenir'>© Powercut Robotics</p>
+      <p className='pl-2 m-2 font-avenir'>© Powercut Robotics</p>
     </footer>
   );
 };
