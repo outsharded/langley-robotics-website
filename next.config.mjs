936e5a9acf7ea6878dbbe844c0fc78ffbe3bd206
outsharded <fab_tec@icloud.com>
2024-07-03 18:21:47 +0100

    headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    headers: {
+        key: 'Cache-Control', value: 'max-age=86400',
+        key: 'themeColour', value:
+            [
+                { media: '(prefers-color-scheme: light)', color: '#f5f5f4' },
+                { media: '(prefers-color-scheme: dark)', color: '#0a0a0a' },
+            ],
+    },
     experimental: {
         staleTimes: {
           dynamic: 300,
